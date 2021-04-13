777d0b761cebe6a87c422eb23cf72339b903c567
Marzeu <marzahell@hotmail.com>
2021-04-13 16:24:06 -0300

    rebase

---
+++ Controllers/ListarController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaEverisDay.Controllers
+{
+    public class ListarController : Controller
+    {
+        public IActionResult Listar()
+        {
+            return View();
+        }
+    }
+}
