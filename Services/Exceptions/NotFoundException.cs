d4057f05df551b3f72e1a36c8fd0c12ed76a93ac
Renan Ranzani <renanranzani@gmail.com>
2021-04-14 09:34:10 -0300

    Excessões

---
+++ Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaEverisDay.Services.Excessoes
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
