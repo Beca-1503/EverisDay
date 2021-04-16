e18efeec42c85e477c1b369649721863df7fc545
Gabriel Benedet <gabrielbenedet78@gmail.com>
2021-04-16 15:53:14 -0300

    Adicionado Endereco ao cliente

--- Models/PizzaContext.cs
+++ Models/PizzaContext.cs
@@ -14,9 +14,10 @@ namespace PizzaEverisDay.Models
         public DbSet<Cliente> Cliente { get; set; }
         public DbSet<Endereco> Endereco { get; set; }
         public DbSet<Produtos> Produtos { get; set; }
+        public DbSet<Cidade> Cidade { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pizza;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-HSEEB7N;Initial Catalog=Pizza;Integrated Security=True");
         }
 
         public DbSet<PizzaEverisDay.Models.Item> Item { get; set; }
